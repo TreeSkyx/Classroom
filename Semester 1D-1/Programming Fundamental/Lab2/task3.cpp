0ffcaf3e8103aa1fc510119d388dc08ecb104180
Wiroon Somphaothong <88649190+TreeSkyx@users.noreply.github.com>
2021-08-15 20:07:23 +0700

    Update task3.cpp
    add condition to create square

--- Semester 1D-1/Programming Fundamental/Lab2/task3.cpp
+++ Semester 1D-1/Programming Fundamental/Lab2/task3.cpp
@@ -4,13 +4,17 @@ int main(){
 	int n,i,j;
 	printf("Enter number : ");
 	scanf("%d",&n);
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			if(i==0 || i==n-1) printf("*");
-			else if(j==0 || j==n-1) printf("*");
-			else if(j!=0 || j!=n-1) printf(" ");		
+	if(n>1) // * doesn't count as a square 
+	{ 
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(i==0 || i==n-1) printf("*");
+				else if(j==0 || j==n-1) printf("*");
+				else if(j!=0 || j!=n-1) printf(" ");		
+			}
+			printf("\n");
 		}
-		printf("\n");
-	}
+	}else printf("error");
+	
 	return 0;
 }
