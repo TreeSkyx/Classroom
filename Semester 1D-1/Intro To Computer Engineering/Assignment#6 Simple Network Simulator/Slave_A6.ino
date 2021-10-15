cefa9a4808fe21a9e5db8dc18fe5477802b32231
Wiroon Somphaothong <88649190+TreeSkyx@users.noreply.github.com>
2021-10-15 19:47:58 +0700

    Add files via upload

---
+++ Semester 1D-1/Intro To Computer Engineering/Assignment#6 Simple Network Simulator/Slave_A6.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+byte num;
+void setup()
+{
+  //Wire.begin(1); // Slave 1
+  Wire.begin(2); // Slave 2
+  Wire.onReceive(receiveData);
+  Wire.onRequest(requestData);
+  Serial.begin(9600); // serial monitor output
+}
+
+void loop()
+{
+  delay(100);
+}
+void receiveData(int howMany)
+{
+  Serial.print("From Master : ");
+  while(1 < Wire.available()) 
+  {
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  num = Wire.read();    // receive byte as an integer
+  Serial.println(num);         // print the integer
+  num+=1;
+}
+void requestData(){
+  Wire.write("Hello");
+  Wire.write(num);
+  Serial.print("Slave 2 To Master : Hello");
+  Serial.println(num);
+}
