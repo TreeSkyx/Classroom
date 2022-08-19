27c17502f4191af27ac411e30fc36fb2e0a8d22e
TreeSkyx <wiroon.som@gmail.com>
2022-08-19 20:20:47 +0700

    Update Queue OODS Lab

---
+++ Semester 2D-1/Object Oriented Data Structures/Queue/02 แถวคอย.py
@@ -0,0 +1,26 @@
+class Queue:
+    def __init__(self,list = None):
+        if list == None:
+            self.items = []
+        else:
+            self.items = list
+
+    def enQueue(self,i):
+        self.items.append(i)
+
+    def deQueue(self):
+        return self.items.pop(0)
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def size(self):
+        return len(self.items)
+
+people, t = input("Enter people and time :").split()
+m = Queue()
+for i in people:
+    m.enQueue(i)
+
+print(m.items)
+
