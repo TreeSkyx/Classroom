0ccd4d444a151c08fbd1684e2ac8fa6b941c0395
TreeSkyx <wiroon.som@gmail.com>
2022-08-22 00:36:37 +0700

    Update 04 เดาใจไว้แล้วว่าเธอรักฉันแบบที่ฉันรัก.py

--- Semester 2D-1/Object Oriented Data Structures/Queue/04 เดาใจไว้แล้วว่าเธอรักฉันแบบที่ฉันรัก.py
+++ Semester 2D-1/Object Oriented Data Structures/Queue/04 เดาใจไว้แล้วว่าเธอรักฉันแบบที่ฉันรัก.py
@@ -20,8 +20,8 @@ class Queue:
 
 day = input("Enter Input : ").split(',')
 
-act = {'0':"Eat", '1':"Game", '2':"Learn", '3':"Movie"}
-location = {'0':"Res.", '1':"ClassR.", '2':"SuperM.", '3':"Home"}
+act = {'0':"Eat", '1':"Game", '2':"Learn", '3':"Movie"}                # activity dict.
+location = {'0':"Res.", '1':"ClassR.", '2':"SuperM.", '3':"Home"}      # loaction dict.
 
 m = Queue()
 y = Queue()
@@ -30,14 +30,16 @@ yAct = Queue()
 mLo = Queue()
 yLo = Queue()
 
+score = 0
+
 for i in day:
     me, you = i.split()
     m.enQueue(me)
     y.enQueue(you)
 
-print("My   Queue = ",end="")
+print("My   Queue = ",end="")   # My Queue
 for i in m.items:
-    a, l = i.split(":")
+    a, l = i.split(":")         # split activity and location
     if i == m.items[0]:
         print(i,end="")
     else:
@@ -45,8 +47,7 @@ for i in m.items:
     mAct.enQueue(act[a])
     mLo.enQueue(location[l])
 
-
-print("\nYour Queue = ",end="")
+print("\nYour Queue = ",end="")     # Your Queue
 for i in y.items:
     a, l = i.split(":")
     if i == y.items[0]:
@@ -56,10 +57,34 @@ for i in y.items:
     yAct.enQueue(act[a])
     yLo.enQueue(location[l])
 
-print("\nMy   Activity:Location = ",end="")
+print("\nMy   Activity:Location = ",end="")     # My Activity & Loaction
 for i in range(mAct.size()):
-    print(mAct.items[i],":",mLo.items[i],end=", ",sep="")
+    if(i == 0):
+        print(mAct.items[i],":",mLo.items[i],end="",sep="")
+    else:
+        print(", ",mAct.items[i],":",mLo.items[i],end="",sep="")       # ref. activity and location using dict.
 
-print("\nYour Activity:Location = ",end="")
+print("\nYour Activity:Location = ",end="")     # Your Activity & Loaction
 for i in range(yAct.size()):
-    print(yAct.items[i],":",yLo.items[i],end=", ",sep="")
+    if(i == 0):
+        print(yAct.items[i],":",yLo.items[i],end="",sep="")
+    else:
+        print(", ",yAct.items[i],":",yLo.items[i],end="",sep="")
+
+### Score Calculation
+for i in range(mAct.size()):
+    if mAct.items[i] == yAct.items[i] and mLo.items[i] == yLo.items[i]:     # same activity and loaction
+        score += 4
+    elif mAct.items[i] != yAct.items[i] and mLo.items[i] == yLo.items[i]:   # different activity / same  loaction
+        score += 2
+    elif mAct.items[i] == yAct.items[i] and mLo.items[i] != yLo.items[i]:   # same activity / different  loaction
+        score += 1
+    else:                                                                   # different activity and location
+        score -= 5
+
+if score >= 7:
+    print("\nYes! You\'re my love! : Score is ", str(score), ".",sep="")
+elif score < 7 and score > 0:
+    print("\nUmm.. It\'s complicated relationship! : Score is ", str(score), ".",sep="")
+elif score < 0:
+    print("\nNo! We\'re just friends. : Score is ", str(score), ".",sep="")
