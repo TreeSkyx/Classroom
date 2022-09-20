a86ac247f6c231766881916c79817e522ee954bb
TreeSkyx <wiroon.som@gmail.com>
2022-09-20 16:10:38 +0700

    Update OODS lab recusion

--- Semester 2D-1/Object Oriented Data Structures/Recusion/02 Length of a String EXTRA.py
+++ Semester 2D-1/Object Oriented Data Structures/Recusion/02 Length of a String EXTRA.py
@@ -1,5 +1,12 @@
-def length(txt):     
-    #Code Here
-    return 
-    
+from itertools import count
+from operator import indexOf
+
+
+def length(txt): 
+    if txt == '':
+        return 0
+    else:
+        print(txt[0],end="",sep='')
+        return 1 + length(txt[1:])
+
 print("\n",length(input("Enter Input : ")),sep="")
