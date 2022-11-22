e498cf4192e4d24c4efda6164acec9cd596835d0
TreeSkyx <wiroon.som@gmail.com>
2022-11-23 00:45:00 +0700

    Update 02 เรียงลำดับโดยไม่สนจำนวนเต็มลบ.py

--- Semester 2D-1/Object Oriented Data Structures/Sorting/02 เรียงลำดับโดยไม่สนจำนวนเต็มลบ.py
+++ Semester 2D-1/Object Oriented Data Structures/Sorting/02 เรียงลำดับโดยไม่สนจำนวนเต็มลบ.py
@@ -1,14 +1,35 @@
 def insertion_sort(array):
     for i in range(1, len(array)):
         key_item = array[i]
-        j = i-1
-        while j >= 0 and array[j] > key_item:
-            array[j+1] = array[j]
-            j -= 1
+        if key_item >= 0:
+            j = i-1
+            while j >= 0 and array[j] > key_item:
+                array[j+1] = array[j]
+                j -= 1
 
-        array[j+1] = key_item
+            array[j+1] = key_item
+        else:
+            continue
     return array
+
+def sortArray(a, n):
+    ans = []
+    for i in range(n):
+        if a[i] >= 0:
+            ans.append(a[i])
+
+    ans = insertion_sort(ans)
+
+    j = 0
+    for i in range(n):
+        if a[i] >= 0:
+            a[i] = ans[j]
+            j += 1
+    
+    for i in range(n):
+        print(a[i],end = " ")
 
 inp = input("Enter Input : ").split()
 num = [int(i) for i in inp]
-print(insertion_sort(num))
+n = len(num)
+sortArray(num,n)
