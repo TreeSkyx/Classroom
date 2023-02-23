b0920acedd63b3d689ffbd1f1cbde8abcef32353
TreeSkyx <88649190+TreeSkyx@users.noreply.github.com>
2023-02-23 07:11:57 +0700

    Add alert box and fix button function

--- Semester 2D-2/Web Application/Lab05/my-app/src/index.js
+++ Semester 2D-2/Web Application/Lab05/my-app/src/index.js
@@ -4,14 +4,30 @@ import "./index.css";
 
 class FoodBox extends React.Component {
   state = {
-    voteMsg: 0,
+    voteMsg: 'Min',
   };
 
   handleCallback = (childData) => {
     if (childData === "1") {
-      this.setState({ voteMsg: this.state.voteMsg + 1 });
+      if (this.state.voteMsg === 9) {
+        this.setState({ voteMsg: "Max" });
+      } else if (this.state.voteMsg === "Max") {
+        alert("Cannot Vote more");
+      } else if (this.state.voteMsg === "Min") {
+        this.setState({ voteMsg: 0 });
+      } else {
+        this.setState({ voteMsg: this.state.voteMsg + 1 });
+      }
     } else {
-      this.setState({ voteMsg: this.state.voteMsg - 1 });
+      if (this.state.voteMsg === 1) {
+        this.setState({ voteMsg: "Min" });
+      } else if (this.state.voteMsg === "Min") {
+        alert("Cannot unvote");
+      } else if (this.state.voteMsg === "Max") {
+        this.setState({ voteMsg: 9 });
+      } else {
+        this.setState({ voteMsg: this.state.voteMsg - 1 });
+      }
     }
   };
 
@@ -21,8 +37,8 @@ class FoodBox extends React.Component {
       <div className="foodBox">
         <div className="grid-container">
           <div className="col1">
-            <h3>{this.props.foodType}</h3>
-            {this.props.foodName}
+            <h3><b>{this.props.foodType}</b></h3>
+            <b>{this.props.foodName}</b>
             <br></br>
             <br></br>
             {this.props.msg}
@@ -31,8 +47,8 @@ class FoodBox extends React.Component {
             <img src={this.props.img} alt=""></img>
           </div>
           <div className="vote">
-            <VoteButton parentCallback={this.handleCallback} />
-            <button>{voteMsg}</button>
+            <VoteButton parentCallback={this.handleCallback} /> &nbsp;
+            <button name="txt">{voteMsg}</button> &nbsp;
             <UnVoteButton parentCallback={this.handleCallback} />
           </div>
         </div>
@@ -68,14 +84,14 @@ class Block extends React.Component {
           <FoodBox
             foodType="อาหารคาว"
             foodName="ข้าวผัด"
-            msg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh nunc, semper at nulla a, venenatis malesuada risus. Aliquam a varius dolor, eget feugiat ante. Mauris ornare enim bibendum, fermentum tellus et, venenatis tortor. Vivamus a vestibulum ex. Etiam tempor purus quis velit luctus pharetra. Ut finibus mauris at semper blandit. Ut eget lectus pellentesque, consequat mauris at, facilisis neque. Maecenas laoreet luctus purus eget molestie. Praesent dui arcu, vulputate eu elit eget, porta placerat eros. Sed vestibulum vestibulum mauris non vestibulum."
+            msg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh nunc, semper at nulla a, venenatis malesuada risus. Aliquam a varius dolor, eget feugiat ante. Mauris ornare enim bibendum, fermentum tellus et, venenatis tortor. Vivamus a vestibulum ex. Etiam tempor purus quis velit luctus pharetra."
             img="https://www.ajinomoto.co.th//storage/photos/shares/our-story/tips/friedrice/62ff47ff5a301.jpg"
           />
           <FoodBox
             foodType="อาหารหวาน"
             foodName="บัวลอย"
-            msg="Vivamus iaculis massa et posuere luctus. Aliquam aliquet eu diam at pulvinar. Mauris egestas venenatis diam, venenatis aliquam mi efficitur ut. Morbi feugiat leo non dapibus finibus. Pellentesque scelerisque lacus ac diam imperdiet, sit amet rhoncus nisl viverra. Nulla hendrerit fermentum nisi, sit amet ullamcorper enim volutpat id. Vestibulum lacinia tempor varius. Etiam suscipit ultrices diam. Nam pulvinar tempus nulla nec imperdiet. Aliquam erat volutpat. Maecenas faucibus ultricies mi, euismod blandit augue accumsan eu. Donec tellus magna, ultricies at lacus ut, dictum ornare justo. Mauris ligula nisi, ullamcorper quis auctor sed, bibendum eu justo."
-            img="https://img.kapook.com/u/2022/wanwanat/1079595941.jpg"
+            msg="Vivamus iaculis massa et posuere luctus. Aliquam aliquet eu diam at pulvinar. Mauris egestas venenatis diam, venenatis aliquam mi efficitur ut. Morbi feugiat leo non dapibus finibus. Pellentesque scelerisque lacus ac diam imperdiet, sit amet rhoncus nisl viverra. Nulla hendrerit fermentum nisi, sit amet ullamcorper enim volutpat id. Vestibulum lacinia tempor varius. Etiam suscipit ultrices diam."
+            img="http://lamphuonline.com/images/00-2022/bua-loi-kaiwan.jpg"
           />
         </div>
       </div>
